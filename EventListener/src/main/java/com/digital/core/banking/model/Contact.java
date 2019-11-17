f66dda8834e0144c7de744e7dca4f8463bc8fe9c
sathishvt <sathishvt@VinsLaptop>
2019-11-17 08:54:04 +0530

    First Commit

---
+++ EventListener/src/main/java/com/digital/core/banking/model/Contact.java
@@ -0,0 +1,5 @@
+package com.digital.core.banking.model;
+
+public class Contact {
+
+}
---
+++ EventListener/src/test/java/com/digital/core/banking/EventListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.core.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
