e621de5e30e5f65157a81f237a2985ad13899ce4
sathishvt <sathishvt@VinsLaptop>
2019-11-17 13:54:24 +0530

    Added Book Sample

---
+++ EventListener/src/main/java/com/digital/core/banking/exception/BookNotFoundException.java
@@ -0,0 +1,14 @@
+package com.digital.core.banking.exception;
+
+public class BookNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BookNotFoundException() {
+
+	}
+
+}
